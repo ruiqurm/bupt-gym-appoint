0b75e13877341810d847378bfdd07cc64634bfc3
ruiqurm <ruiqurm@gmail.com>
2021-05-02 23:15:18 +0800

    1

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function login(name,vpn_pass,general_pass){
               vpn_login_success = true
             }
       }).catch(err=>{
-        throw err;
+        core.setFailed(err)
       });
     if (vpn_login_success===false){
       await rp({
@@ -53,7 +53,7 @@ async function login(name,vpn_pass,general_pass){
         method: 'POST',
         timeout:5000
       }).catch(err=>{
-        throw err
+        core.setFailed(err)
       })
     }
     await rp({
@@ -67,50 +67,51 @@ async function login(name,vpn_pass,general_pass){
       jar:cookiejar
     }).then(res=>{
               // console.log("健身房登录失败");
-              throw "健身房登录失败"
+              core.setFailed("健身房登录失败")
             })
       .catch(res=>{
           if(res.statusCode!==302){
               // console.log("健身房登录失败");
-              throw "健身房登录失败"
+              core.setFailed("健身房登录失败")
           }else{
             console.log("健身房登录成功")
           }
       })
+
   return new Promise((resolve, reject) => {resolve(cookiejar)})
 }
 function appoint(jar,username,date,time){
-  uri = "https://webvpn.bupt.edu.cn/https/77726476706e69737468656265737421f7ee4cd225297a1e73078c/newOrder.php?vpn-12-o2-gym.byr.moe"
-  rp({
-    method: 'POST',
-    uri: uri,
-    form: {
-      "blob": generate_blob(ekey =username,date=date,time=time)
-    },
-    timeout:5000,
-    jar:jar
-  }).then(data=>{
-    if(data==="1"){
-      console.log("成功")
-      status = 1
-    }else if(data==="4"){
-      console.log("已预约过，成功")
-      status = 1
-    }else if(data==="3"){
-      console.log("人数已满，失败")
-      status = 0
-    }else if(data==="6"){
-      console.log("有不良记录，无法预约")
-      status = 0
-    }else{
-      console.log("data="+data+"\n");
-      status = 0
-      throw "成功提交，但是没有预约成功；见data信息\n"
-    }
-  }).catch(err=>{
-      console.log(err);
-      status = 0
-  })
+    uri = "https://webvpn.bupt.edu.cn/https/77726476706e69737468656265737421f7ee4cd225297a1e73078c/newOrder.php?vpn-12-o2-gym.byr.moe"
+    rp({
+      method: 'POST',
+      uri: uri,
+      form: {
+        "blob": generate_blob(ekey =username,date=date,time=time)
+      },
+      timeout:5000,
+      jar:jar
+    }).then(data=>{
+      if(data==="1"){
+        console.log("成功")
+        status = 1
+      }else if(data==="4"){
+        console.log("已预约过，成功")
+        status = 1
+      }else if(data==="3"){
+        console.log("人数已满，失败")
+        status = 0
+      }else if(data==="6"){
+        console.log("有不良记录，无法预约")
+        status = 0
+      }else{
+        console.log("data="+data+"\n");
+        status = 0
+        throw "成功提交，但是没有预约成功；见data信息\n"
+      }
+    }).catch(err=>{
+        status = 0
+        core.setFailed(err)
+    })
 }
 
 var status = 0
@@ -129,7 +130,7 @@ next_date = date.format(next_date,"YYYYMMDD")
 process.env.APPOINT_DATE = next_date
 process.env.APPOINT_HOUR = (time==="1")?"18:40-19:40":((time==="2")?"19:40-20:40":(time==="3"?"20:40-21:40":"未知"))
 console.log("将预约"+next_date+"第"+process.env.APPOINT_HOUR+"时段")
-try{
+
   login(username,vpn_pass,general_pass).then((jar)=>{
     appoint(jar,username,next_date,time)
   }).catch(err=>{
@@ -137,8 +138,6 @@ try{
   }).then((res)=>{
     process.env.APPOINT_STATUS = (status===0)?"失败":"成功"
   })
-}catch(error){
-  core.setFailed("failed:"+error)
-}
+
 
 
